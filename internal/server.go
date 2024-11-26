404d1ee22b71ebb8bb655516db459b3f8c3ae7a2
M4tthewDE <matthias.kronberg@gmail.com>
2024-11-26 22:40:02 +0100

    feat: update memory usage automatically

--- internal/server.go
+++ internal/server.go
@@ -10,18 +10,27 @@ import (
 
 func Run() error {
 	http.HandleFunc("/{$}", root)
+	http.HandleFunc("/memory", mem)
 	http.HandleFunc("/favicon.ico", favicon)
+	http.HandleFunc("/htmx.min.js", htmx)
 
 	return http.ListenAndServe(":8080", nil)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
+	err := template.RootTemplate.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func mem(w http.ResponseWriter, r *http.Request) {
 	memory, err := memory.FetchMemory()
 	if err != nil {
 		log.Println(err)
 	}
 
-	err = template.RootTemplate.Execute(w, memory)
+	err = template.MemoryTemplate.Execute(w, memory)
 	if err != nil {
 		log.Println(err)
 	}
@@ -29,4 +38,8 @@ func root(w http.ResponseWriter, r *http.Request) {
 
 func favicon(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/favicon.ico")
+}
+
+func htmx(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/htmx.min.js")
 }
--- internal/template/template.go
+++ internal/template/template.go
@@ -3,3 +3,4 @@ package template
 import "html/template"
 
 var RootTemplate = template.Must(template.ParseFiles("static/root.html"))
+var MemoryTemplate = template.Must(template.ParseFiles("static/memory.html"))
