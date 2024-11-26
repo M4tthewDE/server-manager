d1cb8a046f160223663947308bbc7cf20ec91380
M4tthewDE <matthias.kronberg@gmail.com>
2024-11-26 18:56:44 +0100

    feat: properly parse memory values

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/m4tthewde/server-manager
 
 go 1.23.3
+
+require github.com/dustin/go-humanize v1.0.1 // indirect
--- internal/memory/memory.go
+++ internal/memory/memory.go
@@ -2,7 +2,10 @@ package memory
 
 import (
 	"os"
+	"strconv"
 	"strings"
+
+	"github.com/dustin/go-humanize"
 )
 
 type Memory struct {
@@ -41,21 +44,27 @@ func FetchMemory() (*Memory, error) {
 
 func parseTotal(text string) (string, error) {
 	line := strings.Split(text, "\n")[0]
-	total := strings.Split(line, ":")[1]
-	total = strings.TrimSpace(total)
-	return total, nil
+	return parse(line)
 }
 
 func parseFree(text string) (string, error) {
 	line := strings.Split(text, "\n")[1]
-	total := strings.Split(line, ":")[1]
-	total = strings.TrimSpace(total)
-	return total, nil
+	return parse(line)
 }
 
 func parseAvailable(text string) (string, error) {
 	line := strings.Split(text, "\n")[2]
-	total := strings.Split(line, ":")[1]
-	total = strings.TrimSpace(total)
-	return total, nil
+	return parse(line)
+}
+
+func parse(line string) (string, error) {
+	rightSide := strings.Split(line, ":")[1]
+	rightSide = strings.TrimSpace(rightSide)
+	rightSide = strings.Split(rightSide, " ")[0]
+	kilobytes, err := strconv.Atoi(rightSide)
+	if err != nil {
+		return "", err
+	}
+
+	return humanize.SIWithDigits(float64(kilobytes)*1000, 3, "B"), nil
 }
